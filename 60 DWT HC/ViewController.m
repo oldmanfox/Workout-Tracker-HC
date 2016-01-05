4ff1bd4da61168062bb60132706bee20e3b99079
Grant, Jared <jared.grant@080-ft-jg-l1.canyonsdistrict.pvt>
2016-01-05 14:32:17 -0700

    Initial Commit

---
+++ 60 DWT HC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  60 DWT HC
+//
+//  Created by Grant, Jared on 1/5/16.
+//  Copyright © 2016 Grant, Luana. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ 60 DWT HC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  60 DWT HC
+//
+//  Created by Grant, Jared on 1/5/16.
+//  Copyright © 2016 Grant, Luana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
